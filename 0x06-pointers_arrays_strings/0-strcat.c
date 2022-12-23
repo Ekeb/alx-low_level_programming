59b6ae4a9e408e882f19da38482d02709e7d8cc7
Ekeb <ekeblessing092@gmail.com>
2022-12-22 19:29:33 -0800

    0-stracat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,23 @@
  * Return: void
  */
 
-char *_strcat (char dest char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	i=0;
-	while (dest [i] !='\0')
+	i = 0;
+	while (dest[i] !='\0')
 	{
-		i++
-	j=0
-	while (src [j] !='\0') 
-
-		dest [i] = src[j]
-
+		i++;
+	}
+	j = 0;
+	while (src[j] !='\0')
+	{
+		dest[i] = src[j];
 		i++;
 		j++;
 	}	
-	dest i = '\0'
+	dest i = '\0';
 	return (dest);
 }
