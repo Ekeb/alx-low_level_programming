72d84b4d8f6a0827fd2d51f2ad0fbe41be38ef54
Ekeb <ekeblessing092@gmail.com>
2022-12-23 15:34:38 -0800

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * -strcat concartenates two stringd
- *  @dest: input value
- *  @src: input value
- *
- * Return: void
+ * -strcat - concartenates two strings
+ *  @dest: copy to
+ *  @src: copy from
+ * Return:pointer to dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (dest[i] !='\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (src[j] !='\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
-	}	
-	dest i = '\0';
+	}
+	dest[i] = '\0';
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar writes the char c to stdout
+ * @c: char to be printed
+ * Return 1 on success
+ */
+it _putchar (char c)
+{
+	return (write 1 &c, 1));
+}
